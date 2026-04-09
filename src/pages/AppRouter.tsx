1e276dd927a713cdc8930b35dce5dab9cf424afc
wei-zuan <269530115+wei-zuan@users.noreply.github.com>
2026-04-09 05:09:17 +0000

    refactor: update vault page naming and router paths

--- src/pages/AppRouter.tsx
+++ src/pages/AppRouter.tsx
@@ -1,13 +1,13 @@
 import { Switch, Route } from "wouter";
 import DashboardPage from "./DashboardPage";
-import CreateQryptankPage from "./CreateQryptankPage";
+import CreateQryptSafePage from "./CreateQryptSafePage";
 import NotFound from "./not-found";
 
 export default function AppRouter() {
     return (
         <Switch>
             <Route path="/" component={DashboardPage} />
-            <Route path="/create" component={CreateQryptankPage} />
+            <Route path="/create" component={CreateQryptSafePage} />
             <Route component={NotFound} />
         </Switch>
     );
